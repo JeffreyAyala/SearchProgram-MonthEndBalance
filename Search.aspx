d81671dfe5010580949f83272c6dc2a6f94cd319
JeffreyAyala <147428884+JeffreyAyala@users.noreply.github.com>
2026-01-17 18:44:27 -0500

    Add files via upload

---
+++ Search.aspx
@@ -0,0 +1,74 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search.aspx.cs" Inherits="MonthEndBalanceSearch.Search" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Month-End Balance Search</title>
+    <style type="text/css">
+        .auto-style1 {
+            font-size: x-large;
+            font-weight: bold;
+        }
+        body {
+            font-family: Arial, sans-serif;
+            margin: 40px;
+        }
+        p {
+            line-height: 1.6;
+        }
+        input[type="text"] {
+            width: 180px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p class="auto-style1">Search Month End Balance </p>
+
+        <p>
+            Last Name
+            <asp:TextBox ID="txtsearch" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;
+            <asp:Button ID="cmdSearch" runat="server" OnClick="Submit_Click" Text="Search" />
+        </p>
+
+        <p>Previous Balance
+            <asp:TextBox ID="txtpreviousbalance" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>Payments
+            <asp:TextBox ID="txtpayments" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>Charges
+            <asp:TextBox ID="txtcharges" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>New Balance
+            <asp:TextBox ID="txtnewbalance" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>Finance Charge
+            <asp:TextBox ID="txtfinancecharge" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>Month End Balance
+            <asp:TextBox ID="txtmonthendbalance" runat="server" ReadOnly="true"></asp:TextBox>
+        </p>
+
+        <p>Picture</p>
+
+        <p>
+            <asp:Image ID="Image1" runat="server" Width="150px" Height="150px" />
+        </p>
+
+        <p>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server"
+                ConnectionString="<%$ ConnectionStrings:MonthEndBalanceSQLConnectionString2 %>"
+                SelectCommand="SELECT [LastName], [PreviousBalance], [Payments], [Charges], [NewBalance], [FinanceCharge], [MonthendBalance], [Picture] FROM [MonthendTable]">
+            </asp:SqlDataSource>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Search.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace MonthEndBalanceSearch
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            string lastname = "", search, picture;
+            double previousBalance = 0, payments = 0, charges = 0, newBalance = 0, financeCharge = 0, monthendBalance = 0;
+            bool found = false;
+
+            search = txtsearch.Text;
+
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["MonthEndBalanceSQLConnectionString2"].ConnectionString);
+            string mySelectQuery = "SELECT LastName, PreviousBalance, Payments, Charges, Picture FROM MonthendTable";
+            SqlCommand command = new SqlCommand(mySelectQuery, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                lastname = reader["LastName"].ToString();
+
+                // Compare names ignoring case and extra spaces
+                if (search.Trim().Equals(lastname.Trim(), StringComparison.OrdinalIgnoreCase))
+                {
+                    previousBalance = Convert.ToDouble(reader["PreviousBalance"]);
+                    payments = Convert.ToDouble(reader["Payments"]);
+                    charges = Convert.ToDouble(reader["Charges"]);
+
+                    // ✅ Apply the correct Charge Account formulas
+                    newBalance = previousBalance - payments + charges;
+                    financeCharge = newBalance * 0.12; // 12%
+                    monthendBalance = newBalance + financeCharge;
+
+                    picture = reader["Picture"].ToString();
+
+                    // ✅ Display the computed results
+                    txtpreviousbalance.Text = previousBalance.ToString("F2");
+                    txtpayments.Text = payments.ToString("F2");
+                    txtcharges.Text = charges.ToString("F2");
+                    txtnewbalance.Text = newBalance.ToString("F2");
+                    txtfinancecharge.Text = financeCharge.ToString("F2");
+                    txtmonthendbalance.Text = monthendBalance.ToString("F2");
+
+                    Image1.ImageUrl = "~/Images/" + picture;
+
+                    found = true;
+                    break;
+                }
+            }
+
+            reader.Close();
+            connection.Close();
+
+            if (!found)
+            {
+                txtpreviousbalance.Text = "";
+                txtpayments.Text = "";
+                txtcharges.Text = "";
+                txtnewbalance.Text = "";
+                txtfinancecharge.Text = "";
+                txtmonthendbalance.Text = "";
+                Image1.ImageUrl = "";
+            }
+        }
+    }
+}
+
